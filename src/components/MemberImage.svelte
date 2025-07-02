4bd21124a82b5356bc5b1a493f301c0b2f4c7245
Itai Epstein <44759817+itaie01@users.noreply.github.com>
2025-07-02 14:31:41 -0400

    initial commit

---
+++ src/components/MemberImage.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Modal from "./Modal.svelte";
+
+  let { imageSource, name, bio } = $props();
+
+  let showModal = $state(false);
+</script>
+
+<button onclick={() => (showModal = true)}>
+  <img
+    src={imageSource}
+    alt={`Image of ${name}`}
+    class="object-contain w-72 h-auto"
+  />
+</button>
+
+<Modal bind:showModal>
+  <img src={imageSource} alt={`Image of ${name}`} />
+  <h1>{name}</h1>
+  <p>{bio}</p>
+</Modal>
