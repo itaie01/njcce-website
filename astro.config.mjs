09013722f970596d3efa9ac54712ca3aa426b324
Itai Epstein <44759817+itaie01@users.noreply.github.com>
2026-03-24 23:31:00 -0400

    deploy to local github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://itaie01.github.io",
+  base: "/njcce-website",
   vite: {
     plugins: [tailwindcss()]
   },
