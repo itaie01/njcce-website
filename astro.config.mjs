253275d9c19dba7910c4c751092d141621a3d057
Itai Epstein <44759817+itaie01@users.noreply.github.com>
2026-05-19 13:05:34 -0400

    add njcce domain to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://itaie01.github.io",
-  base: "/njcce-website",
+  site: "https://www.njcce.org",
+  base: "/",
   vite: {
     // @ts-ignore - vite type mismatch between tailwindcss/vite and astro's bundled vite
     plugins: [tailwind()]
